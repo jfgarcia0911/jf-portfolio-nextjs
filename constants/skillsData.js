4b724d14d8612eacbfa3bacd7c0b2088530f8f18
jfgarcia0911 <jfgarcia0911@gmail.com>
2026-04-09 05:38:24 +0800

    feat: add skillsData constants file for skill icons and titles
    - Define array of skill objects with id, icon component, and title
    - Includes JavaScript, React, Next.js, Node.js, Tailwind, CSS3, Bootstrap, HTML5, Sass, Git, MongoDB, and Figma

---
+++ constants/skillsData.js
@@ -0,0 +1,76 @@
+import JavaScriptIcon from "@/icon/JavaScriptIcon";
+import MongoDbIcon from "@/icon/MongoDbIcon";
+import TailwindIcon from "@/icon/TailwindIcon";
+import NextJsIcon from "@/icon/NextJsIcon";
+import ReactIcon from "@/icon/ReactIcon";
+import BootstrapIcon from "@/icon/BootstrapIcon";
+import Css3Icon from "@/icon/Css3Icon";
+import Html5Icon from "@/icon/Html5Icon";
+import NodeJsIcon from "@/icon/NodeJsIcon";
+import SassIcon from "@/icon/SassIcon";
+import GitIcon from "@/icon/GitIcon";
+import FigmaIcon from "@/icon/FigmaIcon";
+
+
+export const skillsData = [
+	{
+		id: 1,
+		icon: JavaScriptIcon,
+		title: "JavaScript",
+	},
+	{
+		id: 2,
+		icon: ReactIcon,
+		title: "React",
+	},
+	{
+		id: 3,
+		icon: NextJsIcon,
+		title: "Next.js",
+	},
+	{
+		id: 4,
+		icon: NodeJsIcon,
+		title: "Node.js",
+	},
+	{
+		id: 5,
+		icon: TailwindIcon,
+		title: "Tailwind",
+	},
+	{
+		id: 6,
+		icon: Css3Icon,
+		title: "CSS3",
+	},
+	{
+		id: 7,
+		icon: BootstrapIcon,
+		title: "Bootstrap",
+	},
+	{
+		id: 8,
+		icon: Html5Icon,
+		title: "HTML5",
+	},
+	{
+		id: 9,
+		icon: SassIcon,
+		title: "Sass",
+	},
+	{
+		id: 10,
+		icon: GitIcon,
+		title: "Git",
+	},
+	{
+		id: 11,
+		icon: MongoDbIcon,
+		title: "MongoDB",
+	},
+	{
+		id: 12,
+		icon: FigmaIcon,
+		title: "Figma",
+	},
+];
