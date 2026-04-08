26575cb635413bbb109206ac37bec5f18259f8a6
jfgarcia0911 <jfgarcia0911@gmail.com>
2026-04-09 05:37:25 +0800

    feat: add navIcons constants file for social media links
    - Import icon components (GithubIcon, FacebookIcon, LinkedInIcon, TwitterIcon)
    - Define array with href and ariaLabel for each social platform

---
+++ constants/navIcons.js
@@ -0,0 +1,12 @@
+// constants/navIcons.js
+import FacebookIcon from "@/icon/FacebookIcon";
+import LinkedInIcon from "@/icon/LinkedInIcon";
+import GithubIcon from "@/icon/GithubIcon";
+import TwitterIcon from "@/icon/TwitterIcon";
+
+export const navIcons = [
+  { icon: GithubIcon, href: "https://www.github.com", ariaLabel: "Github" },
+  { icon: FacebookIcon, href: "https://www.facebook.com", ariaLabel: "Facebook" },
+  { icon: LinkedInIcon, href: "https://www.linkedin.com", ariaLabel: "LinkedIn" },
+  { icon: TwitterIcon, href: "https://www.twitter.com", ariaLabel: "Twitter" },
+];
