3916e26a2210751d77df49cb1c9fdbb352f792f5
jfgarcia0911 <jfgarcia0911@gmail.com>
2026-04-10 04:53:25 +0800

    feat: add dark mode support and integrate layout components
    - Convert Home component to client component with dark mode state
    - Pass isDarkMode and setIsDarkMode to Navbar for theme toggling
    - Pass isDarkMode to all section components (Hero, About, Skills, Projects, Contact, Footer)
    - Apply conditional background color (gray-900 for dark, white for light) to main element

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,26 @@
-import Image from "next/image";
-import Hero from "@/components/Hero"
-import About from "@/components/About"
-import Skills from "@/components/Skills"
-import Projects from "@/components/Projects"
-import Contact from "@/components/Contact"
-import Footer from "@/components/Footer"
+"use client";
+import Navbar from "@/components/layout/Navbar";
+import Hero from "@/components/layout/Hero";
+import About from "@/components/layout/About";
+import Skills from "@/components/layout/Skills";
+import Projects from "@/components/layout/Projects";
+import Contact from "@/components/layout/Contact";
+import Footer from "@/components/layout/Footer";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <main className='bg-gray-900'>
-      <Hero/>
-      <About/>
-      <Skills/>
-      <Projects/>
-      <Contact/>
-      <Footer/>
-    </main>
-  );
+	const [isDarkMode, setIsDarkMode] = useState("dark");
+	return (
+		<main
+			className={`${isDarkMode === "dark" ? "bg-gray-900" : "bg-white"}   scroll-smooth`}
+		>
+			<Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+			<Hero isDarkMode={isDarkMode} />
+			<About isDarkMode={isDarkMode} />
+			<Skills isDarkMode={isDarkMode} />
+			<Projects isDarkMode={isDarkMode} />
+			<Contact isDarkMode={isDarkMode} />
+			<Footer isDarkMode={isDarkMode} />
+		</main>
+	);
 }
